64d57e5671abb00138a5ae527c81bf3182fe8e18
Alan1203TI <167146297+Alan1203TI@users.noreply.github.com>
2026-06-22 15:36:57 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,6 +1,4 @@
-// PODSESI Player - Configuração do Firebase
-
-export const firebaseConfig = {
+const firebaseConfig = {
   apiKey: "AIzaSyAXGS1MxNPkL-4BU1VO4esMBiYhOQ_Pq0g",
   authDomain: "podsesi2026.firebaseapp.com",
   projectId: "podsesi2026",
@@ -8,5 +6,3 @@ export const firebaseConfig = {
   messagingSenderId: "663234484955",
   appId: "1:663234484955:web:540f776565468a46780afe"
 };
-
-export const ADMIN_PASSWORD = "podsesi2026";
